a6c5a4710d94ea213b2a5462892b185b7b04e3c6
rajender <rajender2sr@gmail.com>
2023-05-31 23:12:31 +0530

    roboshop nginx script

---
+++ user.sh
@@ -0,0 +1,39 @@
+echo -e "\e[33mconfiguring nodejs repos\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+
+echo -e "\e[33minstalling nodejs\e[0m"
+yum install nodejs -y &>>/tmp/roboshop.log
+
+echo -e "\e[33mAdd application User\e[0m"
+useradd roboshop &>>/tmp/roboshop.log
+echo -e "\e[33mcreate application directory\e[0m"
+rm -rf /app &>>/tmp/roboshop.log
+mkdir /app &>>/tmp/roboshop.log
+
+echo -e "\e[33mDownload the application code\e[0m"
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
+
+echo -e "\e[33mextracting the application\e[0m"
+unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
+
+echo -e "\e[33mdownload the dependencies\e[0m"
+npm install &>>/tmp/roboshop.log
+
+echo -e "\e[33mSetup SystemD Catalogue Service\e[0m"
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service &>>/tmp/roboshop.log
+
+echo -e "\e[33mstart user service\e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable user &>>/tmp/roboshop.log
+systemctl start user &>>/tmp/roboshop.log
+
+echo -e "\e[33mcopy mongodb repo file\e[0m"
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+
+echo -e "\e[33minstall mongodb client\e[0m"
+yum install mongodb-org-shell -y &>>/tmp/roboshop.log
+
+echo -e "\e[33mLoad Schema\e[0m"
+mongo --host mongodb-dev.sraji73.store </app/schema/user.js &>>/tmp/roboshop.log
