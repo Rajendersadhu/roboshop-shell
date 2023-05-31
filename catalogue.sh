c01b344da52badb6549880b88e16ee85e6cbd08f
rajender <rajender2sr@gmail.com>
2023-05-31 13:14:50 +0530

    roboshop nginx script

---
+++ catalogue.sh
@@ -0,0 +1,41 @@
+echo -e "\e[33mconfiguring nodejs repos\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+
+echo -e "\e[33minstalling nodejs\e[0m"
+yum install nodejs -y &>>/tmp/roboshop.log
+
+echo -e "\e[33mAdd application User\e[0m"
+useradd roboshop &>>/tmp/roboshop.log
+echo -e "\e[33mcreate application directory\e[0m"
+rm -rf /app &>>/tmp/roboshop.log
+mkdir /app &>>/tmp/roboshop.log
+
+echo -e "\e[33mDownload the application code\e[0m"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
+
+echo -e "\e[33mextracting the application\e[0m"
+unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
+
+echo -e "\e[33mdownload the dependencies\e[0m"
+npm install &>>/tmp/roboshop.log
+
+echo -e "\e[33mSetup SystemD Catalogue Service\e[0m"
+cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+
+echo -e "\e[33mstart catalogue service\e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable catalogue &>>/tmp/roboshop.log
+systemctl start catalogue &>>/tmp/roboshop.log
+
+echo -e "\e[33mcopy mongodb repo file\e[0m"
+cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+
+echo -e "\e[33minstall mongodb client\e[0m"
+yum install mongodb-org-shell -y &>>/tmp/roboshop.log
+
+echo -e "\e[33mLoad Schema\e[0m"
+mongo --host mongodb-dev.sraji73.store </app/schema/catalogue.js &>>/tmp/roboshop.log
+
+
--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,24 @@
 echo -e "\e[33minstalling nginx server\e[0m"
 yum install nginx -y &>>/tmp/roboshop.log
+
 echo -e "\e[33mremove the old app content\e[0m"
 rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
+
 echo -e "\e[33mdownload the frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
+
 echo -e "\e[33mextract the frontend content\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
-## we need to copy config file
+
+echo -e "\e[33mupdate frontend configuration\e[0m"
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 echo -e "\e[33mstart nginx server\e[0m"
 systemctl enable nginx &>>/tmp/roboshop.log
 systemctl restart nginx &>>/tmp/roboshop.log
+
+
 
 
 
