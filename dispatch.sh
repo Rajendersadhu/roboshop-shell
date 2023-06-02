9fe3210ea149a5b3187c7f672175bd8bfd0c6aed
rajender <rajender2sr@gmail.com>
2023-06-02 19:39:28 +0530

    dispatch script

--- dispatch.sh
+++ dispatch.sh
@@ -1,34 +1,34 @@
 echo -e "\e[33mInstall GoLang\e[0m"
-yum install golang -y
+yum install golang -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mAdd application Use\e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[33msetup an app directory\e[0m"
-mkdir /app
+mkdir /app &>>/tmp/roboshop.log
 
 
 echo -e "\e[33mDownload the application content\e[0m"
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
-cd /app
+curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
 
 
 echo -e "\e[33mextracting the application\e[0m"
-unzip /tmp/dispatch.zip
+unzip /tmp/dispatch.zip &>>/tmp/roboshop.log
 
 echo -e "\e[33mdownload dispatch dependencies\e[0m"
-cd /app
-go mod init dispatch
-go get
-go build
+cd /app &>>/tmp/roboshop.log
+go mod init dispatch &>>/tmp/roboshop.log
+go get &>>/tmp/roboshop.log
+go build &>>/tmp/roboshop.log
 
 echo -e "\e[33mSetup SystemD dispatch Service\e[0m"
-cp /home/centos/roboshop-shell/dispatch.service /etc/systemd/system/dispatch.service
+cp /home/centos/roboshop-shell/dispatch.service /etc/systemd/system/dispatch.service &>>/tmp/roboshop.log
 
 echo -e "\e[33mLoad dispatch service\e[0m"
-systemctl daemon-reload
+systemctl daemon-reload &>>/tmp/roboshop.log
 
 echo -e "\e[33mstart dispatch service\e[0m"
-systemctl enable dispatch
-systemctl start dispatch
+systemctl enable dispatch &>>/tmp/roboshop.log
+systemctl start dispatch &>>/tmp/roboshop.log
 
