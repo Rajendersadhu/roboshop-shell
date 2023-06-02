55dd336ecee3daf6ee79dd7ef5044f6e20b5ade8
rajender <rajender2sr@gmail.com>
2023-06-02 16:54:38 +0530

    roboshop nginx script

---
+++ cart.sh
@@ -0,0 +1,37 @@
+echo -e "\e[33mconfiguring nodejs repos\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+
+echo -e "\e[33minstalling nodejs\e[0m"
+yum install nodejs -y &>>/tmp/roboshop.log
+
+echo -e "\e[33mAdd application User\e[0m"
+useradd roboshop &>>/tmp/roboshop.log
+echo -e "\e[33mcreate application directory\e[0m"
+rm -rf /app &>>/tmp/roboshop.log
+mkdir /app
+echo -e "\e[33mDownload the application code\e[0m"
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
+
+echo -e "\e[33mextracting the application\e[0m"
+unzip /tmp/cart.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
+
+echo -e "\e[33mdownload the dependencies\e[0m"
+npm install &>>/tmp/roboshop.log
+
+echo -e "\e[33mSetup SystemD cart Service\e[0m"
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service &>>/tmp/roboshop.log
+
+echo -e "\e[33mstart cart service\e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable cart &>>/tmp/roboshop.log
+systemctl start cart &>>/tmp/roboshop.log
+
+echo -e "\e[33mcopy mongodb repo file\e[0m"
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
+
+echo -e "\e[33minstall mongodb client\e[0m"
+yum install mongodb-org-shell -y &>>/tmp/roboshop.log
+echo -e "\e[33mLoad Schema\e[0m"
+mongo --host mongodb-dev.sraji73.store </app/schema/cart.js &>>/tmp/roboshop.log
