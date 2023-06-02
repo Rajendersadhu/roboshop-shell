6b1fc0a78f67ec36c49970eb7b38bdec0b3e0ec7
rajender <rajender2sr@gmail.com>
2023-06-02 17:35:37 +0530

    roboshop nginx script

--- mysql.sh
+++ mysql.sh
@@ -1,19 +1,20 @@
 echo -e "\e[33mdisable MySQL 8 version\e[0m"
-yum module disable mysql -y
+yum module disable mysql -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mSetup the MySQL5.7 repo file\e[0m"
 mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
 
 echo -e "\e[33mInstall MySQL Server\e[0m"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mStart MySQL Service\e[0m"
-systemctl enable mysqld
-systemctl start mysqld
+systemctl enable mysqld &>>/tmp/roboshop.log
+systemctl start mysqld &>>/tmp/roboshop.log
 
 echo -e "\e[33m change the default root password\e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
 
  echo -e "\e[33mcheck the new password\e[0m"
-mysql -uroot -pRoboShop@1
+mysql -uroot -pRoboShop@1 &>>/tmp/roboshop.log
+
 
