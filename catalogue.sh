e48f8fba9d0387c7ed1221f4fdd15f4059c596b0
rajender <rajender2sr@gmail.com>
2023-06-03 13:10:55 +0530

    variable basic

--- catalogue.sh
+++ catalogue.sh
@@ -2,36 +2,7 @@ source common.sh
 component=catalogue
 
 
-echo -e "${color} configuring nodejs repos ${nocolor}"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
 
-echo -e "${color} installing nodejs ${nocolor}"
-yum install nodejs -y &>>$log_file
-
-echo -e "${color} Add application User ${nocolor}"
-useradd roboshop &>>$log_file
-echo -e "${color} create application directory ${nocolor}"
-rm -rf ${app_path} &>>$log_file
-mkdir ${app_path} &>>$log_file
-
-echo -e "${color} Download the application code ${nocolor}"
-curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
-cd ${app_path} &>>$log_file
-
-echo -e "${color} extracting the application ${nocolor}"
-unzip /tmp/$component.zip &>>$log_file
-cd ${app_path} &>>$log_file
-
-echo -e "${color} download the dependencies ${nocolor}"
-npm install &>>$log_file
-
-echo -e "${color} Setup SystemD Catalogue Service ${nocolor}"
-cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>$log_file
-
-echo -e "${color} start catalogue service ${nocolor}"
-systemctl daemon-reload &>>$log_file
-systemctl enable$component &>>$log_file
-systemctl start $component &>>$log_file
 
 echo -e "${color} copy mongodb repo file ${nocolor}"
 cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>$log_file
--- common.sh
+++ common.sh
@@ -3,3 +3,36 @@ color="\e[35m"
 nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
+
+nodejs() {
+  echo -e "${color} configuring nodejs repos ${nocolor}"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
+
+  echo -e "${color} installing nodejs ${nocolor}"
+  yum install nodejs -y &>>$log_file
+
+  echo -e "${color} Add application User ${nocolor}"
+  useradd roboshop &>>$log_file
+  echo -e "${color} create application directory ${nocolor}"
+  rm -rf ${app_path} &>>$log_file
+  mkdir ${app_path} &>>$log_file
+
+  echo -e "${color} Download the application code ${nocolor}"
+  curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
+  cd ${app_path} &>>$log_file
+
+  echo -e "${color} extracting the application ${nocolor}"
+  unzip /tmp/$component.zip &>>$log_file
+  cd ${app_path} &>>$log_file
+
+  echo -e "${color} download the dependencies ${nocolor}"
+  npm install &>>$log_file
+
+  echo -e "${color} Setup SystemD Catalogue Service ${nocolor}"
+  cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>$log_file
+
+  echo -e "${color} start catalogue service ${nocolor}"
+  systemctl daemon-reload &>>$log_file
+  systemctl enable$component &>>$log_file
+  systemctl start $component &>>$log_file
+}
