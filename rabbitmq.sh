266d02418eb1712d8125d663f1155b65f3751d6a
rajender <rajender2sr@gmail.com>
2023-06-05 10:27:45 +0530

    variable basic

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,26 +1,29 @@
 
 
+
 source common.sh
+component=$component
+
 echo -e "${color}configure erlang repos ${nocolor}"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>/tmp/roboshop.log
+curl -s https://packagecloud.io/install/repositories/$component/erlang/script.rpm.sh | bash &>>/tmp/roboshop.log
 stat_check $?
 
-echo -e "${color}configure rabbitmq repos ${nocolor}"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>/tmp/roboshop.log
+echo -e "${color}configure $component repos ${nocolor}"
+curl -s https://packagecloud.io/install/repositories/$component/$component-server/script.rpm.sh | bash &>>/tmp/roboshop.log
 stat_check $?
 
-echo -e "${color}Install RabbitMQ ${nocolor}"
-yum install rabbitmq-server -y &>>/tmp/roboshop.log
+echo -e "${color}Install $component ${nocolor}"
+yum install $component-server -y &>>/tmp/roboshop.log
 stat_check $?
 
-echo -e "${color}Start RabbitMQ Service ${nocolor}"
-systemctl enable rabbitmq-server &>>/tmp/roboshop.log
-systemctl start rabbitmq-server &>>/tmp/roboshop.log
+echo -e "${color}Start $component Service ${nocolor}"
+systemctl enable $component-server &>>/tmp/roboshop.log
+systemctl start $component-server &>>/tmp/roboshop.log
 stat_check $?
 
-echo -e "${color}add rabbitmq application user ${nocolor}"
-rabbitmqctl add_user roboshop $1 &>>/tmp/roboshop.log
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.log
+echo -e "${color}add $component application user ${nocolor}"
+$componentctl add_user roboshop $1 &>>/tmp/roboshop.log
+$componentctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.log
 stat_check $?
 
 
